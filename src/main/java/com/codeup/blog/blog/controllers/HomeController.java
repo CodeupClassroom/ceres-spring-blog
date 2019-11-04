e720160796325c4b3e56dec38058fddbd8100177
Fernando Mendoza <fernandoza@gmail.com>
2019-11-04 14:20:42 -0600

    feat: exercises 1 and 2

---
+++ src/main/java/com/codeup/blog/blog/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.codeup.blog.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index(){
+        return "This is the landing page!";
+    }
+}
---
+++ src/main/java/com/codeup/blog/blog/controllers/MathController.java
@@ -0,0 +1,41 @@
+package com.codeup.blog.blog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String add(@PathVariable int num1, @PathVariable int num2){
+        int total = num1 + num2;
+        return String.valueOf(total);
+    }
+
+    @GetMapping("/subtract/{num1}/from/{num2}")
+    @ResponseBody
+    public String subtract(@PathVariable int num1, @PathVariable int num2){
+        return String.valueOf(num1 - num2);
+    }
+
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiply(@PathVariable int num1, @PathVariable int num2){
+        return String.valueOf(num1 * num2);
+    }
+
+    @GetMapping("/divide/{num1}/by/{num2}")
+    @ResponseBody
+    public String divide(@PathVariable double num1, @PathVariable double num2){
+
+        if(num2 == 0){
+            return num1 + " cannot be divided by Zero";
+        }
+
+        return String.valueOf(num1 / num2);
+    }
+
+}
