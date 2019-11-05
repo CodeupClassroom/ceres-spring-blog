7d21dee7b9909399bf7f34b905aaa9e1de942bfd
Fernando Mendoza <fernandoza@gmail.com>
2019-11-05 11:17:38 -0600

    feat: examples with objects and templating

--- src/main/java/com/codeup/blog/blog/controllers/HomeController.java
+++ src/main/java/com/codeup/blog/blog/controllers/HomeController.java
@@ -1,14 +1,27 @@
 package com.codeup.blog.blog.controllers;
 
+import com.codeup.blog.blog.Ad;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model viewModel){
+
+        ArrayList<Ad> adsList = new ArrayList<Ad>();
+
+        adsList.add(new Ad(1,"first ad", "new"));
+        adsList.add(new Ad(2,"second ad", "new"));
+        adsList.add(new Ad(3,"third ad", "used"));
+
+        viewModel.addAttribute("ads", adsList);
+
         return "home";
     }
 }
